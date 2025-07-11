604c62c4dd236ee9dbe5a3535b13b4db51e2d612
Manimaran K <manimaran230806@gmail.com>
2025-07-11 10:44:42 +0530

    Time: 4 ms (46.94%), Space: 19.7 MB (21.81%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,12 @@
-class Solution(object):
-    def singleNumber(self, nums):
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d={}
         for i in nums:
-            if nums.count(i)==1:
+            if i in d:
+                d[i]=d[i]+1
+            else:
+                d[i]=1
+        for i in d:
+            if d[i]==1:
                 return i
-        
+            
