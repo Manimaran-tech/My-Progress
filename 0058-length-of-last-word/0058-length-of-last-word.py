35021e31274837273989b76e528e6c70c123f1e1
Manimaran K <manimaran230806@gmail.com>
2025-11-06 21:20:06 +0530

    Time: 0 ms (100%), Space: 17.6 MB (98.39%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,5 +1,4 @@
-class Solution(object):
-    def lengthOfLastWord(self, s):
-        v=s.strip().split()
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        v= s.split()
         return len(v[-1])
-        
