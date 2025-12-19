c9f6ea61b43a59a05110c8cd79b9ce4cae09ea1a
Manimaran K <manimaran230806@gmail.com>
2025-12-19 18:10:29 +0530

    Time: 0 ms (100%), Space: 17.1 MB (100%) - LeetHub

--- 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.py
+++ 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.py
@@ -5,7 +5,7 @@ class Solution:
 
         for i in range(n):
             if s[i] == '?':
-                for ch in 'abc':   # only 3 letters are enough
+                for ch in 'abc':   
                     prev_ok = (i == 0 or s[i - 1] != ch)
                     next_ok = (i == n - 1 or s[i + 1] != ch)
                     if prev_ok and next_ok:
