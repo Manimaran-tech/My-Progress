58f2960ec184d44e0dd7a2b26fabbf6fa2f77dfe
Manimaran K <manimaran230806@gmail.com>
2025-12-30 21:55:29 +0530

    Time: 46 ms (56.75%), Space: 18.7 MB (93.73%) - LeetHub

---
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        
+        depth = 0
+        q = deque([root])
+        
+        while q:
+            depth += 1
+            for _ in range(len(q)):
+                node = q.popleft()
+                for child in node.children:
+                    q.append(child)
+        
+        return depth
