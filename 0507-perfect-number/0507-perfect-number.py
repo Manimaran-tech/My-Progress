c88c1c642658da4ac97e7b238b621555f0351a76
Manimaran K <manimaran230806@gmail.com>
2026-02-02 19:13:42 +0530

    Time: 9 ms (9.06%), Space: 19.3 MB (46.56%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        
+        if num <= 1:
+            return False
+        
+        divisor_sum = 1  # 1 is always a divisor
+        
+        i = 2
+        while i * i <= num:
+            if num % i == 0:
+                divisor_sum += i
+                if i != num // i:
+                    divisor_sum += num // i
+            i += 1
+        
+        return divisor_sum == num
